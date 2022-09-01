c20f4c06ef89e226e0650b0e789cfb54123b8ec5
aneskak <aneska.karina@hotmail.com>
2022-09-01 20:26:59 -0300

    first commit

---
+++ js/typed.js
@@ -0,0 +1,7 @@
+
+    var typed = new Typed('.typing-text', {
+
+        strings: [ 'Software Developer', 'Quality Assurance Tester', 'Front-end Developer'],
+        typeSpeed: 120,
+        loop: true
+    });
